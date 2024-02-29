fcd42e38b57b6398e8d1b06253de8dd2a337ecb4
EdwardSoon <edward.soonrw@shopee.com>
2024-02-29 15:04:26 +0800

    GroupAnagrams

---
+++ GroupAnagrams/src/GroupAnagrams.java
@@ -0,0 +1,45 @@
+/*
+1. if meet similar case then add into the existed arraylist
+2. else, create a new arraylist
+ */
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, ArrayList<String>> anagramGroupedStrMap = new HashMap<>();
+        ArrayList<String> uniqueAnagramList = new ArrayList<>();
+
+        for (String str : strs){
+            char[] anagramCharArr = str.toCharArray();  // convert each string to charArray so that we can sort
+            Arrays.sort(anagramCharArr);
+            String anagram = String.valueOf(anagramCharArr);  //convert back the sorted charArray into String
+            if (!anagramGroupedStrMap.containsKey(anagram)){
+                uniqueAnagramList.add(anagram);  // store the anagram into a list for later to iterate over to the hashMap value into finalList
+                anagramGroupedStrMap.put(anagram, new ArrayList<String>(List.of(str))); // for each unique anagram as key, we create an ArrayList to group the string with similar anagram together.
+                // To initialise ArrayList, it takes a collection of lists
+                // we need ArrayList because List is just an interface without concrete implementation.
+            }
+            else { // if the map contains the anagram
+                anagramGroupedStrMap.get(anagram).add(str); // get the ArrayList (Value), then add the str inside.
+            }
+        }
+        List<List<String>> finalList = new ArrayList<>();  // needs to return as List, but List is the interface of ArrayList, so can be initiated as ArrayList, but must be List Type
+        for (String anagram : uniqueAnagramList){
+            finalList.add(anagramGroupedStrMap.get(anagram));
+        }
+        return finalList;
+    }
+
+
+    public static void main(String[] args) {
+        String[] x = {"eat","tea","tan","ate","nat","bat"};
+        System.out.print(groupAnagrams(x));
+
+    }
+}
